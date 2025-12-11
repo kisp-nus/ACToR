79cf862f14b694c5aa15d0c4f36699ed551de4a6
lty12b9b0a1 <lty19b@gmail.com>
2025-12-11 17:59:01 +0800

    fixed a bug: one benchmark project "projects_input_BSD/env/" is wrongly ignored by gitignore due to `env/` pattern, which is used to ignore potential environment files. Such pattern does not exist anymore, so can rm.

---
+++ projects_input_BSD/env/util.h
@@ -0,0 +1,8 @@
+// === copied from compat/include/compat.h BEGIN (Line 53-56) ===
+extern const char *__progname;
+// === copied from compat/include/compat.h END (Line 53-56) ===
+
+// === copied from compat/common/progname.c BEGIN (Line 15-116) ===
+const char* getprogname(void);
+void setprogname(const char *progname);
+// === copied from compat/common/progname.c END (Line 15-116) ===
